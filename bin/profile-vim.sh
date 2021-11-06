369962aba6e225dcaaf5b82f122caa7f0be6e1fa
Ammar Alakkad <am.alakkad@gmail.com>
2021-11-06 19:43:57 +0300

    Add profile-vim script

---
+++ bin/profile-vim.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Credits to: https://gist.github.com/disusered/af7c1bf0add302946bf1
+
+iterations=10
+
+# -----------------------------------------------------------------------------
+# Create array of results
+
+declare -a results
+
+for i in $(seq 1 $iterations);
+do
+  nvim --startuptime vim.log -c 'q'
+  latest=$(awk '/./{line=$0} END{print line}' vim.log | awk '{ print $1}')
+  results+=( $latest )
+done
+
+# -----------------------------------------------------------------------------
+# Calculate average
+
+total=0
+for delta in "${results[@]}"
+do
+  total=`echo $total + $delta | bc -l`
+done
+
+average=`echo $total / $iterations | bc -l`
+echo $average
+rm vim.log
