a9a43b8175b694685d7ea27c2fb9a39af8e8fc19
Ammar Alakkad <am.alakkad@gmail.com>
2023-12-07 08:41:05 +0300

    Switch to lazyvim.org

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,2 @@
---[[
-
-Inspirations:
-- https://github.com/brainfucksec/neovim-lua
-- https://github.com/folke/dot
---]]
-
-vim.g.mapleader = ","
-vim.g.maplocalleader = ","
-
--- Import Lua modules
-require("lazy_init")
-require("core/options")
-require("core/autocmds")
-require("core/keymaps")
-
-vim.api.nvim_create_user_command("Fold", function()
-	-- Fold current buffer based on treesitter
-	-- https://www.jmaguire.tech/posts/treesitter_folding/
-	local opt = vim.opt
-	opt.foldmethod = "expr"
-	opt.foldexpr = "nvim_treesitter#foldexpr()"
-end, {})
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,47 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+
+-- Highlight on yank
+-- remove auto command added by LazyVim
+vim.api.nvim_clear_autocmds({ event = "TextYankPost" })
+autocmd("TextYankPost", {
+  group = augroup("highlight_yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ higroup = "Search", timeout = "1000" })
+  end,
+})
+
+-- Don't auto commenting new lines
+autocmd("BufEnter", {
+  pattern = "*",
+  command = "set fo-=c fo-=r fo-=o",
+})
+
+-- Enter insert mode when switching to terminal
+-- autocmd("TermOpen", {
+--   command = "setlocal listchars= nonumber norelativenumber nocursorline",
+-- })
+--
+autocmd("TermOpen", {
+  pattern = "*",
+  command = "startinsert",
+})
+
+-- Close terminal buffer on process exit
+autocmd("BufLeave", {
+  pattern = "term://*",
+  command = "stopinsert",
+})
+
+-- show cursor line only in active window
+local cursorGrp = augroup("CursorLine", { clear = true })
+autocmd({ "InsertLeave", "WinEnter" }, {
+  pattern = "*",
+  command = "set cursorline",
+  group = cursorGrp,
+})
+autocmd({ "InsertEnter", "WinLeave" }, { pattern = "*", command = "set nocursorline", group = cursorGrp })
