6587c121284d660ba57ae5fd221f2c1d42ec9d10
Ammar Alakkad <am.alakkad@gmail.com>
2024-02-22 09:12:53 +0300

    nvim: add lazyvim tailwind

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     -- import/override with your plugins
     { import = "plugins" },
   },
