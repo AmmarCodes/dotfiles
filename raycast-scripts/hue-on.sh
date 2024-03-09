6d869ab55e745ca6dff12b3de338c982a61400b7
Ammar Alakkad <am.alakkad@gmail.com>
2024-03-09 11:10:11 +0300

    Add xbar milestone plugin

---
+++ raycast-scripts/hue-on.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title lights on
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 💡
+
+# Documentation:
+# @raycast.description Turn office lights on
+# @raycast.author AmmarCodes
+# @raycast.authorURL https://raycast.com/AmmarCodes
+
+source ~/.private_exports
+ip="192.168.1.2"
+group_id=82
+
+url="https://$ip/api/$HUE_USERNAME/groups/$group_id/action"
+
+curl -k -s -H "Accept: application/json" -X PUT --data "{\"on\": true}" $url &>/dev/null
+
+if [[ $? != 0 ]]; then
+	echo "Could not turn it on"
+fi
---
+++ xbar/plugins/milestone.12h.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+# Metadata allows your plugin to show up in the app, and website.
+#
+#  <xbar.title>Milestone days left</xbar.title>
+#  <xbar.version>v1.0</xbar.version>
+#  <xbar.author>Ammar Alakkad</xbar.author>
+#  <xbar.author.github>AmmarCodes</xbar.author.github>
+#  <xbar.desc>Displays how many days left for the current GitLab milestone</xbar.desc>
+#  <xbar.image></xbar.image>
+#  <xbar.dependencies>ruby</xbar.dependencies>
+#  <xbar.abouturl>https://ammar.codes</xbar.abouturl>
+
+require 'open-uri'
+require 'yaml'
+require 'date'
+
+# Fetch YAML file from URL
+url = 'https://gitlab.com/gitlab-com/www-gitlab-com/-/raw/master/data/releases.yml'
+file_contents = URI.open(url).read
+
+# Parse YAML
+data = YAML.safe_load(file_contents)
+
+# Sort releases by date
+sorted_releases = data.sort_by { |release| Time.parse(release['date']) }
+
+# Find the current release
+current_date = Time.now
+current_release = sorted_releases.find { |release| Time.parse(release['date']) > current_date }
+
+# Calculate work days until release (assuming release is 6 days earlier)
+if current_release
+  release_date = Date.parse(current_release['date']) - 6
+  work_days_left = (release_date - Date.today).to_i
+  output = "📆 #{work_days_left} days"
+  output << " | color=red" if work_days_left < 7
+  output << "\n---\n"
+  output << "#{current_release['version']} last day: #{release_date}"
+  puts output
+end
