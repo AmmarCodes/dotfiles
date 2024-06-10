c3a89179e7ffb91a3a3860139130c0814e050855
Ammar Alakkad <am.alakkad@gmail.com>
2024-06-10 12:56:29 +0300

    nvim: add inc-rename extra and update lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.editor.inc-rename" },
 
     -- import/override with your plugins
     { import = "plugins" },
