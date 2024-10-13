6239b150d093e48d08ca17abe58777f6cbba2de8
Ammar Alakkad <am.alakkad@gmail.com>
2024-10-13 08:32:01 +0300

    yazi: add packages

--- fish/config.fish
+++ fish/config.fish
@@ -76,6 +76,15 @@ function fco -d "Use `fzf` to choose which branch to check out" --argument-names
     git for-each-ref --format='%(refname:short)' refs/heads | fzf --height 10% --layout=reverse --border --query=$branch --select-1 | xargs git checkout
 end
 
+function y -d "Start yazi (TUI file manager)"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 ###########
 # Exports #
 ###########
