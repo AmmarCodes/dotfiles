3ca2e5f2f375c3a594d68a80056a46804729cde5
Ammar Alakkad <am.alakkad@gmail.com>
2024-11-29 09:28:02 +0300

    nvim: use nvim-ufo for folding

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
 vim.g.mapleader = ","
 local opt = vim.opt
 
@@ -9,8 +9,5 @@ opt.swapfile = false -- Don't use swapfile
 opt.wildignore = "*/.git/*,*/.hg/*,*/.svn/*,*/.DS_Store"
 opt.confirm = false -- Confirm to save changes before exiting modified buffer
 opt.background = "dark"
-
--- opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
--- opt.foldtext = "v:lua.vim.treesitter.foldtext()"
 
 vim.g.lazyvim_php_lsp = "intelephense"
