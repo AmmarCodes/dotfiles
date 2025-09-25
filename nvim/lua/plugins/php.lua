f4bfb88146c74c0e77cc836b33dd99df8f90522f
Ammar Alakkad <am.alakkad@gmail.com>
2025-09-25 09:29:10 +0300

    nvim: comment-out php changes

--- nvim/lua/plugins/php.lua
+++ nvim/lua/plugins/php.lua
@@ -14,34 +14,34 @@ return {
   --     ensure_installed = { "pint", "phpstan" },
   --   },
   -- },
-  {
-    -- Add a Treesitter parser for Laravel Blade to provide Blade syntax highlighting.
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "blade",
-        "php_only",
-      })
-    end,
-    config = function(_, opts)
-      vim.filetype.add({
-        pattern = {
-          [".*%.blade%.php"] = "blade",
-        },
-      })
-
-      require("nvim-treesitter.configs").setup(opts)
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
-    end,
-  },
+  -- {
+  --   -- Add a Treesitter parser for Laravel Blade to provide Blade syntax highlighting.
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "blade",
+  --       "php_only",
+  --     })
+  --   end,
+  --   config = function(_, opts)
+  --     vim.filetype.add({
+  --       pattern = {
+  --         [".*%.blade%.php"] = "blade",
+  --       },
+  --     })
+  --
+  --     require("nvim-treesitter.configs").setup(opts)
+  --     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+  --     parser_config.blade = {
+  --       install_info = {
+  --         url = "https://github.com/EmranMR/tree-sitter-blade",
+  --         files = { "src/parser.c" },
+  --         branch = "main",
+  --       },
+  --       filetype = "blade",
+  --     }
+  --   end,
+  -- },
   -- {
   --   -- Add the blade-nav.nvim plugin which provides Goto File capabilities
   --   -- for Blade files.
