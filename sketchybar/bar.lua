54430bcbf8c6dcd32d6664357e2cede8a596d217
Ammar Alakkad <am.alakkad@gmail.com>
2025-10-17 10:18:52 +0300

    aerpspace and jankybar updates

--- sketchybar/bar.lua
+++ sketchybar/bar.lua
@@ -2,14 +2,14 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-  topmost = "window",
-  height = 28,
-  color = colors.bar.bg,
-  margin = 8,
-  corner_radius = 6,
-  shadow = false,
-  blur_radius = 10,
-  padding_right = 0,
-  padding_left = 0,
-  y_offset = 3,
+	topmost = "window",
+	height = 24,
+	color = colors.bar.bg,
+	margin = 4,
+	corner_radius = 6,
+	shadow = false,
+	blur_radius = 10,
+	padding_right = 0,
+	padding_left = 0,
+	y_offset = 0,
 })
