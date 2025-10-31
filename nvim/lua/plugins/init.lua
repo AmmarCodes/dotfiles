2b4175448d533d167d9f99e356f2650f87490707
Ammar Alakkad <am.alakkad@gmail.com>
2025-10-31 19:14:21 +0300

    feat(nvim): switch back to gitlab official plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -260,7 +260,6 @@ return {
   {
     "https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git",
     event = { "BufReadPre", "BufNewFile" },
-    enabled = false,
     cond = function()
       -- Only activate if token is present in environment variable.
       -- Remove this line to use the interactive workflow.
